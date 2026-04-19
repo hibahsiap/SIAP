7cc9ae7c901fb55090120753f15521644b2189bc
Fathoni1509 <139218185+Fathoni1509@users.noreply.github.com>
2026-04-19 14:48:31 +0700

    feat: login page (#1)
    merge login page

---
+++ components/Button.tsx
@@ -0,0 +1,19 @@
+interface ButtonClickProps {
+    name: string,
+    type?: "submit" | "reset" | "button",
+    className?: string,
+    disabled?: boolean
+}
+
+const ButtonClick = ({name, type = "submit", className="", disabled}: ButtonClickProps) => {
+    return (
+        <button 
+            type={type} 
+            className={`bg-linear-to-br from-[#1D2F58] to-[#041942] rounded-lg w-full text-white font-medium text-[10px] tracking-widest uppercase py-3 disabled:bg-gray-400 disabled:cursor-not-allowed ${className}`}
+        >
+            {name}
+        </button>
+    )
+}
+
+export default ButtonClick;
