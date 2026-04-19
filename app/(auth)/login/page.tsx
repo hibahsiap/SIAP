b5f9d33c1376edad13a07ff68ddbee930a5cbea1
Fathoni1509 <habibifathoni1509@gmail.com>
2026-04-19 20:27:42 +0700

    fix: fixing directory and route

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import Login from "@/components/Login";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-[#f9f9f9] font-sans py-18 px-66">
+      <Login/>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,5 @@
-// app/page.tsx
-import Sidebar from "@/components/sidebar";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen">
-      <Sidebar />
-      <div className="p-10">
-        <h1>Dashboard Utama</h1>
-      </div>
-    </main>
-import Login from "@/components/Login";
-
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-[#f9f9f9] font-sans py-18 px-68">
-      <Login/>
-    </div>
-  );
+  redirect('/login');
 }
