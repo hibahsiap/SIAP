3be390189b47a60380ad820482a0dd4adb85c61c
dachill19 <daffa.hasanal@gmail.com>
2026-05-17 02:22:24 +0700

    refactor: update sidebar menu keys to uppercase constants

--- constants/sidebar-menu.tsx
+++ constants/sidebar-menu.tsx
@@ -1,12 +1,12 @@
 import { Inbox, Kanban, FileText, Ticket, MessageSquare, Share2, Users, FileChartColumn, UserRoundCog } from 'lucide-react';
 
 export const SIDEBAR_MENU = {
-  admin: [
+  ADMIN: [
     { name: 'Tickets', href: '/admin/dashboard/tickets', icon: <Ticket size={20} /> },
     { name: 'Reports', href: '/admin/dashboard/reports', icon: <FileChartColumn size={20} /> },
     { name: 'User Management', href: '/admin/dashboard/user-management', icon: <UserRoundCog size={20} /> },
   ],
-  opd: [
+  OPD: [
     { name: 'Inbox', href: '/opd/dashboard/inbox', icon: <MessageSquare size={20} /> },
     { name: 'Tickets', href: '/opd/dashboard/tickets', icon: <Ticket size={20} /> },
     { name: 'Reports', href: '/opd/dashboard/reports', icon: <FileChartColumn size={20} /> },
